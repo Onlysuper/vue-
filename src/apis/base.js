b94f6ade7946acf95c7a6543584ab188e91c17a6
apple <apple@apples-MacBook-Pro.local>
2019-03-28 17:49:08 +0800

    配置测试环境

--- src/apis/base.js
+++ src/apis/base.js
@@ -12,14 +12,5 @@ function getUrl() {
     }
 export default {
         md5Data:"ZzJ2A2r_H1D9LJ589DY",
-        // //公众号服务
-        // oaIp: process.env.NODE_ENV == "development" || process.env.NODE_ENV == "productionTest" ? location.protocol + "//to.yeepiao.com/" : location.protocol + "//o.yeepiao.com/",
-        //快速开票
-        testIp: process.env.NODE_ENV == "development" || process.env.NODE_ENV == "productionTest" ? location.protocol + "//ti.yeepiao.com/" : location.protocol + "//i.yeepiao.com/",
-        //开票码验签的Key
-        // signKey: process.env.NODE_ENV == "development" || process.env.NODE_ENV == "productionTest" ? "1234QWER" : "",
-        
-        //展示商户服务
-        // oaIp: process.env.NODE_ENV == "development" || process.env.NODE_ENV == "productionTest" ? location.protocol + "//easy-mock.liuup.com/mock/5c98365fd360c27a130ae4bc/" : location.protocol + "//o.yeepiao.com/",
         oaIp: getUrl()
 }
