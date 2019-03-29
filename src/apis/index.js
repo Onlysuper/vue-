b27e361cdbb6ef342417f9903475108ee0084f76
apple <apple@apples-MacBook-Pro.local>
2019-03-29 11:58:06 +0800

    .do

--- src/apis/index.js
+++ src/apis/index.js
@@ -4,22 +4,22 @@ import base from "./base";
 
 
 // 登录获取验证码
-const getVerificationCode = () => { return (params) => { return http.postJSON(base.oaIp, "/ChnpayPubAcct/realMerPubNum/sendMsgCode.html", params) } }
+const getVerificationCode = () => { return (params) => { return http.postJSON(base.oaIp, "/ChnpayPubAcct/realMerPubNum/sendMsgCode.do", params) } }
 // 商户登录
-const login = () => { return (params) => { return http.postJSON(base.oaIp, "/ChnpayPubAcct/realMerPubNum/userLogin.html", params) } }
+const login = () => { return (params) => { return http.postJSON(base.oaIp, "/ChnpayPubAcct/realMerPubNum/userLogin.do", params) } }
 // 获取当前手机号下的商户列表
-const getCustomerList = () => { return (params) => { return http.postJSON(base.oaIp, "/ChnpayPubAcct/realMerPubNum/queryMerList.html", params) } }
+const getCustomerList = () => { return (params) => { return http.postJSON(base.oaIp, "/ChnpayPubAcct/realMerPubNum/queryMerList.do", params) } }
 // 获取商户信息
-const getCustomerInfo = () => { return params => { return http.postJSON(base.oaIp, "/ChnpayPubAcct/realMerPubNum/queryMerDetailInfo.html", params) } }
+const getCustomerInfo = () => { return params => { return http.postJSON(base.oaIp, "/ChnpayPubAcct/realMerPubNum/queryMerDetailInfo.do", params) } }
 //退出登录
-const logout = () => { return params => { return http.postJSON(base.oaIp, "/ChnpayPubAcct/realMerPubNum/userLoginOut.html", params) } }
+const logout = () => { return params => { return http.postJSON(base.oaIp, "/ChnpayPubAcct/realMerPubNum/userLoginOut.do", params) } }
 // 分页查询交易记录列表
-const payOrderQueryList = () => { return params => { return http.postJSON(base.oaIp, "/ChnpayPubAcct/realMerPubNum/queryMerTranList.html", params) } }
+const payOrderQueryList = () => { return params => { return http.postJSON(base.oaIp, "/ChnpayPubAcct/realMerPubNum/queryMerTranList.do", params) } }
 
 // 查询今日交易统计
-const payOrderTodayTotal = () => { return params => { return http.postJSON(base.oaIp, "/ChnpayPubAcct/realMerPubNum/queryMerTodayTran.html", params) } }
+const payOrderTodayTotal = () => { return params => { return http.postJSON(base.oaIp, "/ChnpayPubAcct/realMerPubNum/queryMerTodayTran.do", params) } }
 // 查询商户结算记录
-const settleQuery = () => { return (params) => { return http.postJSON(base.oaIp, `/ChnpayPubAcct/realMerPubNum/queryMerSettList.html`, params, false) } }
+const settleQuery = () => { return (params) => { return http.postJSON(base.oaIp, `/ChnpayPubAcct/realMerPubNum/queryMerSettList.do`, params, false) } }
 
 
 
