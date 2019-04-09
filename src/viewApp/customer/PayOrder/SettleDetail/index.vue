28c2eeb216932c159c3a8c5f9f1cd25e86cb94e0
apple <apple@apples-MacBook-Pro.local>
2019-04-09 17:03:32 +0800

    出款状态详情修改

--- src/viewApp/customer/PayOrder/SettleDetail/index.vue
+++ src/viewApp/customer/PayOrder/SettleDetail/index.vue
@@ -1,18 +1,11 @@
 <template>
         <div class="pay-order-detail">
                 <input-wrapper>
-                        <!-- <m-cell title="商户号">{{item.merCode}}</m-cell>
-                        <m-cell title="商户名">{{item.merName}}</m-cell> -->
                         <m-cell title="结算日期">{{item.settDate | dateFilter}}</m-cell>
-                        <!-- <m-cell title="共收款">{{}}元</m-cell>
-                        <m-cell title="手续费">{{}}元</m-cell> -->
                         <m-cell title="结算金额">{{item.settAmt | moneyFormatCN(true)}}元</m-cell>
                         <m-cell title="账户名称">{{item.merSettAcctName}}</m-cell>
                         <m-cell title="结算卡号">{{item.merSettAcct | accountNum}}</m-cell>
-                        <m-cell title="出款状态">{{item.outState | analyFilter(CONST,'outMoneyStatus-show','issort')}}</m-cell>
-                        <!-- <m-cell title="结算银行">{{item.settBank}}</m-cell> -->
-                        <!-- <m-cell title="结算类型">{{item.settType}}</m-cell> -->
-                        <!-- <m-cell title="结算类型">{{item.settType || analyFilter(CONST,'settTypes','issort')}}</m-cell> -->
+                        <m-cell title="出款状态">{{item.outState | analyFilter()}}</m-cell>
                 </input-wrapper>
         </div>
 </template>
@@ -33,6 +26,19 @@ export default {
                         CONST:CONST,
                         item: this.$route.query
                 };
+        },
+        filters: {
+                analyFilter: function (value) {
+                       if(value=='55'){
+                               // 成功
+                               return "出款成功"
+                       }else if(value=='44'){
+                               // 失败
+                               return "出款失败"
+                       }else{
+                                return "出款中"
+                       }
+                }
         },
         mounted() {
         },
